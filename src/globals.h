1afd5aa83ac0100d10c365f9935287e1098c7fc3
hlpr98 <hlpr98@gmail.com>
2020-01-14 09:41:13 +0530

    malloc.h

---
+++ src/globals.h
@@ -0,0 +1,7 @@
+#ifndef __NVM_GLOBALS__
+#define __NVM_GLOBALS__
+
+#include <stdlib.h>
+#include <string.h>
+
+#endif // !__NVM_GLOBALS__
--- src/malloc.h
+++ src/malloc.h
@@ -1,11 +1,30 @@
-/*
-* This file contains the definitions of all the functions related to 
-* `Heap allocation` of variables.
-*/
-
 #ifndef __NVM_MALLOC__
 #define __NVM_MALLOC__
 
+/*
+* This file contains the definitions of all the functions related to 
+* `Heap allocation` of variables.
+*
+* Idea :
+*                    (if to be sent to NVRAM)
+*                    ........................  libpmem.io
+*                   /
+* nvmmalloc() ------
+*                   \........................  malloc()
+*                    (if to be sent to DRAM)
+*                     
+*/
+
+#include "globals.h"
+
+typedef struct nvmalloc_rt {
+
+} nvmalloc;
+
+// The top level function for allocation of space in heap.
+nvmalloc nvmmalloc(size_t size);
+
+nvmalloc nvmmalloc(size_t size, void **data);
 
 
-#endif
+#endif // !__NVM_MALLOC__
