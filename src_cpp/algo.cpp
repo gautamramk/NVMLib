021f7cc823a80180742b43f24eeec6d802d05c9b
Gautam Ramakrishnan <gautamramk@gmail.com>
2020-01-28 08:26:55 +0530

    algo.cpp start

--- src_cpp/algo.cpp
+++ src_cpp/algo.cpp
@@ -3,3 +3,20 @@
 template <typename T>
 static std::unordered_map <struct nvmalloc<T>*, nvmalloc<T>> alloc_table;
 
+int decide_allocation () {
+
+}
+
+template <typename T>
+T& retrieve_value(nvmalloc <T> obj) {
+    switch (obj.type_of_allocation()) {
+        case NVRAM_HEAP:
+            break;
+        case NVRAM_STACK:
+            break;
+        case DRAM_HEAP:
+            break;
+        case DRAM_STACK:
+            break;
+    }
+}
