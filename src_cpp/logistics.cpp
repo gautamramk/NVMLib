13d13fc49f8517ada02008140b1c6f2fffd082e4
hlpr98 <hlpr98@gmail.com>
2020-02-06 13:07:25 +0530

    update to logistics.cpp

--- src_cpp/logistics.cpp
+++ src_cpp/logistics.cpp
@@ -1,15 +1,67 @@
 #include <unordered_map>
+#include <libpmemobj/p.hpp>
+#include <libpmemobj/persistent_ptr.hpp>
+#include <libpmemobj++/persistent_ptr_base.hpp>
 #include <libpmemobj.h>
+#include <memory>
+#include "logistics.hpp"
 
-template <typename T>
-static std::unordered_set <__UINT64_TYPE__> alloc_table;
+
+static std::unordered_map <__UINT64_TYPE__, pmem::obj::persistent_ptr_base> alloc_table_nvram;
+static std::unordered_map <__UINT64_TYPE__, std::unique_ptr> alloc_table_dram;
 
 void init_hashmap(){
-    alloc_table = new std::unordered_set<struct nvmalloc<T>*>();
+    alloc_table_nvram = new std::unordered_map<__UINT64_TYPE__, pmem::obj::persistent_ptr_base>();
+    alloc_table_dram = new std::unordered_map<__UINT64_TYPE__, std::unique_ptr>();
 }
 
-void add_new_object(__UINT64_TYPE__ key){
-    alloc_table.insert(key);
+template <typename T>
+void add_new_object(__UINT64_TYPE__ key, __UINT16_TYPE__ type_of_allocation){
+    const nvmalloc<T> nvm_obj = *((nvmalloc<T>*)key);
+
+    alloc_table[key] = type_of_allocation - NVRAM_HEAP ? create_dram_obj(nvm_obj.get_size()) : create_nvram_obj(nvm_obj.get_size());
+}
+
+template <typename T>
+void add_new_object(__UINT64_TYPE__ key, __UINT16_TYPE__ type_of_allocation, const T &data) {
+
+    alloc_table[key] = type_of_allocation - NVRAM_HEAP ? create_dram_obj(data) : create_nvram_obj(data);
+
+    store_data(key, data);
+}
+
+// how to get the type
+template <typename T>
+static std::unique_ptr create_dram_obj(const T &data) {
+    std::unique_ptr<T> new_ptr((T*)malloc(data));
+
+    return new_ptr;
+}
+
+
+template <typename T>
+static pmem::obj::persistent_ptr_base create_nvram_obj(const T &_data) {
+
+    if (std::is_class(T)) {
+        struct root {
+          pmem::obj::persistent_ptr<T> data;
+        };
+
+        pmem::obj::persistent_ptr<root> rootp = pmemobj_root(pop, sizeof(root));
+    } else {
+        struct actual_datatype {
+          pmem::obj::p<T> var; 
+        };
+
+        struct root {
+          pmem::obj::persistent_ptr<T> data;
+        };
+
+        pmem::obj::persistent_ptr<root> rootp = pmemobj_root(pop, sizeof(root));
+    }
+    
+
+    return rootp;
 }
 
 template <typename T>
@@ -17,7 +69,7 @@ int store_data(__UINT64_TYPE__ key, const T &data){
     if (alloc_table.find(key) == alloc_table.end())
         return -1;
     else {
-        nvmalloc<T>* ptr = reinterpret_cast<nvmalloc<T>*>(key);
+        nvmalloc<T>* ptr = (nvmalloc<T>*)key;
         *(ptr->data) = data;
     }
     return 0;
--- src_cpp/logistics.hpp
+++ src_cpp/logistics.hpp
@@ -1,16 +1,19 @@
 #ifndef __LOGISTICS_NVM__
 #define __LOGISTICS_NVM__
 
+#include "globals.hpp"
+#include "malloc.hpp"
+
 void init_hashmap();
 
 // adding the new object to hash table
-void add_new_object(__UINT64_TYPE__ key);  // calls create new obj based on obj.type_of_allocation
+void add_new_object(__UINT64_TYPE__ key, __UINT16_TYPE__ type_of_allocation);  // calls create new obj based on obj.type_of_allocation
 
 template <typename T>
 int store_data(__UINT64_TYPE__ key, const T &data);
 
 void* dram_pointer_return(__UINT64_TYPE__ key);
 
-PMEMoid nvram_pointer_return(__UINT64_TYPE__ key);
+void* nvram_pointer_return(__UINT64_TYPE__ key);
 
 #endif __LOGISTICS_NVM__ // !__LOGISTICS_NVM__ 
