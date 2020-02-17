60146c99907ce69ce6e450453c699393f19282fe
Gautam Ramakrishnan <gautamramk@gmail.com>
2020-02-17 16:19:39 +0530

    Debugged generic lists.

--- src_c_new/list.h
+++ src_c_new/list.h
@@ -1,20 +1,23 @@
 #ifndef __NVM_LIST__
 #define __NVM_LIST__
 
+#include <stdlib.h>
+
+#define NULL 0
 #define DECLARE_LIST(VALUE_TYPE)\
-/* Defines the list node for a generic linked list */\
-typedef struct VALUE_TYPE##_list_node {\
-    VALUE_TYPE##* ele;\
-    VALUE_TYPE##_list_node* next;\
-    VALUE_TYPE##_list_node* prev;\
-} VALUE_TYPE##_list_node;\
+struct VALUE_TYPE##_list_node {\
+    VALUE_TYPE* ele;\
+    struct VALUE_TYPE##_list_node* next;\
+    struct VALUE_TYPE##_list_node* prev;\
+};\
 \
-/* Defines the list head for a generic linked list */\
-typedef struct VALUE_TYPE##_list_head {\
-    VALUE_TYPE##_list_node* curr;\
-    VALUE_TYPE##_list_node* first;\
-    VALUE_TYPE##_list_node* last;\
-} VALUE_TYPE##_list_head;\
+struct VALUE_TYPE##_list_head {\
+    struct VALUE_TYPE##_list_node* curr;\
+    struct VALUE_TYPE##_list_node* first;\
+    struct VALUE_TYPE##_list_node* last;\
+};\
+typedef struct VALUE_TYPE##_list_node VALUE_TYPE##_list_node;\
+typedef struct VALUE_TYPE##_list_head VALUE_TYPE##_list_head;\
 \
 VALUE_TYPE##_list_head* create_##VALUE_TYPE##_list() {\
     VALUE_TYPE##_list_head* new_head = (VALUE_TYPE##_list_head*)malloc(sizeof(VALUE_TYPE##_list_head));\
@@ -23,7 +26,7 @@ VALUE_TYPE##_list_head* create_##VALUE_TYPE##_list() {\
     new_head->last = NULL;\
 }\
 \
-void insert_head_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_head* list_head) {\
+void insert_head_##VALUE_TYPE##_node(VALUE_TYPE* ele, VALUE_TYPE##_list_head* list_head) {\
     VALUE_TYPE##_list_node* new_node = (VALUE_TYPE##_list_node*)malloc(sizeof(VALUE_TYPE##_list_node));\
     new_node->ele = ele;\
     new_node->next = list_head->first;\
@@ -36,7 +39,7 @@ void insert_head_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_head* 
     list_head->first = new_node;\
 }\
 \
-void insert_tail_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_head* list_head) {\
+void insert_tail_##VALUE_TYPE##_node(VALUE_TYPE* ele, VALUE_TYPE##_list_head* list_head) {\
     VALUE_TYPE##_list_node* new_node = (VALUE_TYPE##_list_node*)malloc(sizeof(VALUE_TYPE##_list_node));\
     new_node->ele = ele;\
     new_node->prev = list_head->last;\
@@ -49,7 +52,7 @@ void insert_tail_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_head* 
     list_head->last = new_node;\
 }\
 \
-void insert_after_curr_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_head* list_head) {\
+void insert_after_curr_##VALUE_TYPE##_node(VALUE_TYPE* ele, VALUE_TYPE##_list_head* list_head) {\
     if (!list_head->curr) {\
         insert_head_##VALUE_TYPE##_node(ele, list_head);\
         return;\
@@ -66,7 +69,7 @@ void insert_after_curr_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_
     new_node->prev = list_head->curr;\
 }\
 \
-void insert_before_curr_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list_head* list_head) {\
+void insert_before_curr_##VALUE_TYPE##_node(VALUE_TYPE* ele, VALUE_TYPE##_list_head* list_head) {\
     if (!list_head->curr) {\
         insert_tail_##VALUE_TYPE##_node(ele, list_head);\
         return;\
@@ -83,12 +86,12 @@ void insert_before_curr_##VALUE_TYPE##_node(VALUE_TYPE##* ele, VALUE_TYPE##_list
     new_node->next = list_head->curr;\
 }\
 \
-VALUE_TYPE##* access_##VALUE_LIST##_list_current(VALUE_TYPE##_list_head* list_head) {\
+VALUE_TYPE* access_##VALUE_TYPE##_list_current(VALUE_TYPE##_list_head* list_head) {\
     return list_head->curr->ele;\
 }\
 \
 void VALUE_TYPE##_list_next_ele(VALUE_TYPE##_list_head* list_head) {\
-    if (list_head->curr && list_head->curr->next) {\
+    if (list_head->curr) {\
         list_head->curr = list_head->curr->next;\
     }\
 }\
@@ -96,10 +99,11 @@ void VALUE_TYPE##_list_next_ele(VALUE_TYPE##_list_head* list_head) {\
 void VALUE_TYPE##_list_prev_ele(VALUE_TYPE##_list_head* list_head) {\
     if (list_head->curr && list_head->curr->prev) {\
         list_head->curr = list_head->curr->prev;\
-    }\
+    } else if (!list_head->curr)\
+        list_head->curr = list_head->last;\
 }\
 \
-void VALUE_TYPE##_list_del_curr(VALUE_TYPE##_list_head* list_head){\
+void VALUE_TYPE##_list_del_curr(VALUE_TYPE##_list_head* list_head) {\
     if (list_head->curr) {\
         if (list_head->curr->prev)\
             list_head->curr->prev->next = list_head->curr->next;\
@@ -116,6 +120,10 @@ void VALUE_TYPE##_list_del_curr(VALUE_TYPE##_list_head* list_head){\
     } else {\
         list_head->last = NULL;\
     }\
+}\
+\
+void VALUE_TYPE##_list_goto_head(VALUE_TYPE##_list_head* list_head) {\
+    list_head->curr = list_head->first;\
 }\
 
 #endif // !__NVM_LIST__
---
+++ src_c_new/list_debug_example.c
@@ -0,0 +1,29 @@
+#include "list.h"
+#include <stdio.h>
+
+DECLARE_LIST(int)
+
+void main() {
+    int_list_head* list_head = create_int_list();
+    int* numbers = (int*)malloc(20*sizeof(int));
+    for (int i = 0; i < 20; i++) {
+        numbers[i] = i;
+        if (i < 10)
+            insert_tail_int_node(&numbers[i], list_head);
+    }
+    int_list_goto_head(list_head);
+    while (list_head->curr != NULL) {
+        printf("%d\n", *access_int_list_current(list_head));
+        int_list_next_ele(list_head);
+    }
+    int_list_prev_ele(list_head);
+    printf("%d\n", *access_int_list_current(list_head));
+    insert_before_curr_int_node(&numbers[11], list_head);
+    int_list_prev_ele(list_head);
+    printf("%d\n", *access_int_list_current(list_head));
+    printf("_______\n");
+    while (list_head->curr != NULL) {
+        printf("%d\n", *access_int_list_current(list_head));
+        int_list_next_ele(list_head);
+    }
+}
