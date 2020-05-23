77b90fc73a30538d7bff48a29185c596888d78ea
hlpr98 <hlpr98@gmail.com>
2020-05-23 14:11:34 +0530

    Added the primary user header

---
+++ src_c_new/initializer.h
@@ -0,0 +1,12 @@
+#ifndef __NVM_INITIALIZER__
+#define __NVM_INITIALIZER__
+
+#include "globals.h"
+
+// The function the user has to call before any of his/her code
+//
+// This must be the firse line in the `main` function.
+void initialize();
+
+
+#endif // !__NVM_INITIALIZER__
