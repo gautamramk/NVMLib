ad2e762c8a6c7e1a733ef89823c152583d3b449c
Gautam Ramakrishnan <gautamramk@gmail.com>
2020-05-27 01:28:09 +0530

    bubble sort examples

---
+++ examples/bubble_sort.c
@@ -0,0 +1,63 @@
+#include <libNVMlib.h>
+#include <unistd.h>
+
+LIB_TOID_DECLARE(int);
+
+int main() {
+    initialize();
+    srand(time(NULL));
+    clock_t end, start;
+    start = clock();
+    int size = 1000;
+    LIB_TOID(int) arr = (LIB_TOID(int))memalloc(size * sizeof(int), NVRAM_HEAP);
+    LIB_TOID(int) flag = (LIB_TOID(int))memalloc(sizeof(int), NVRAM_HEAP);
+    LIB_TOID(int) idx = (LIB_TOID(int))memalloc(sizeof(int), NVRAM_HEAP);
+    LIB_TOID(int) idy = (LIB_TOID(int))memalloc(sizeof(int), NVRAM_HEAP);
+
+    int i =  *LIB_D_RO(idx) < size ? *LIB_D_RO(idx):0;
+    if(*LIB_D_RO(flag) == 1) {
+        goto skip_init;
+    }
+
+    for(int i = 0; i< size; i++) {
+        LIB_D_RW(arr)[i] = rand()%100000;
+    }
+
+    *LIB_D_RW(flag) = 1;
+
+skip_init:
+    //printf("Starting skip init.\n");
+
+   // int i = *LIB_D_RO(idx);
+
+    for (; i < size - 1; i++) {
+        int j =  *LIB_D_RO(idy) < size - i - 1? *LIB_D_RO(idy):0;
+        //int j = *LIB_D_RO(idy);
+        for (; j < size - i - 1; j++) {
+            //int temp1 = LIB_D_RO(arr)[j];
+            //int temp2 = LIB_D_RO(arr)[j+1];
+            
+            //if (temp1 > temp2) {
+            if (LIB_D_RO(arr)[j] > LIB_D_RO(arr)[j+1]) {
+                int temp = LIB_D_RO(arr)[j+1];
+                LIB_D_RW(arr)[j + 1] = LIB_D_RO(arr)[j];
+                LIB_D_RW(arr)[j] = temp;
+            }
+            *LIB_D_RW(idy) = j;
+        }
+        *LIB_D_RW(idx) = i;
+        //printf("Array at iteration %d is: ", i);
+        //for (int k = 0; k < size; k++) {
+            // printf("%d, ", LIB_D_RO(arr)[k]);
+        // }
+        // printf("\n");
+        // sleep(1);
+    }
+    end = clock();
+    printf("Elapsed time = %f", (double)(end - start)/CLOCKS_PER_SEC);
+    memfree(arr);
+    memfree(flag);
+    memfree(idx);
+    memfree(idy);
+    sleep(5);
+}
---
+++ examples/new_bubble_sort.c
@@ -0,0 +1,50 @@
+// C program for implementation of Bubble sort 
+#include <stdio.h> 
+#include <time.h> 
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+// A function to implement bubble sort 
+void bubbleSort(int *arr, int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+  
+       // Last i elements are already in place    
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) 
+              swap(&arr[j], &arr[j+1]); 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    srand(time(NULL));
+    clock_t end, start;
+    start = clock();
+    int size = 100;
+    int* arr = (int*)malloc(size*sizeof(int));
+    for(int i = 0; i < size; i++){
+        arr[i] = rand()%100000;
+    }
+
+    bubbleSort(arr, size);
+    end = clock();
+    printf("Elapsed time = %f", (double)(end - start)/CLOCKS_PER_SEC); 
+    //printf("Sorted array: \n"); 
+    //printArray(arr, size); 
+    return 0; 
+} 
